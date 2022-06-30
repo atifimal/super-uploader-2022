cce96e7e69c9c8f973e0a90b525cdfd07856d983
Atif Imal <mehmetatif@mail.ru>
2022-06-30 22:07:17 +0300

    Added empty projects · Spring Boot + React

---
+++ back-end/src/main/java/com/atifimal/task/superuploader2022/SuperUploader2022Application.java
@@ -0,0 +1,13 @@
+package com.atifimal.task.superuploader2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperUploader2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperUploader2022Application.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/atifimal/task/superuploader2022/SuperUploader2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atifimal.task.superuploader2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperUploader2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
