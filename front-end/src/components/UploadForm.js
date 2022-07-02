cc622f09354a55ebaf695ccec80cfa40a852c7cf
Atif Imal <mehmetatif@mail.ru>
2022-07-02 16:32:02 +0300

    FRONT-END · Configured formData and fetch/post functions

--- front-end/src/components/UploadForm.js
+++ front-end/src/components/UploadForm.js
@@ -1,19 +1,27 @@
 function UploadForm() {
+    let file;
     const fileChangedHandler = (event) => {
-        let file_size = event.target.files[0].size;
-        let file_name = event.target.files[0].name;
-        let file_type = event.target.files[0].type;
-    
-       console.log(event.target.files[0]);
+        file = event.target.files[0];
+        console.log(event.target.files[0]);
     };
 
+    const formDataPost = () => {
+        let formData = new FormData();
+        formData.append('file', file);
+        let result = fetch('http://localhost:8080/upload-file', {
+            mode: 'no-cors',
+            method: 'POST',
+            body: formData
+        });
+    }
+
     return (
-        <form className="form-inline" action="#" method="POST">
+        <form className="form-inline">
             <div className="form-group m-3">
                 <label htmlFor="formFile" className="form-label mx-3">File</label>
-                <input className="form-control d-inline w-auto" type="file" id="formFile" name="file" onChange={fileChangedHandler}/>
+                <input className="form-control d-inline w-auto" type="file" id="formFile" onChange={fileChangedHandler} />
             </div>
-            <input type={"submit"} className="btn btn-primary m-3" value="Send"/>
+            <input type={"button"} className="btn btn-primary m-3" value="Send" onClick={formDataPost} />
         </form>
     )
 }
